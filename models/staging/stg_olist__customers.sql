f852ac7b1590c81cb996a2917dbe34a8cc3d992c
Mark Smolders <marksmolders09@gmail.com>
2026-07-18 10:07:52 +0000

    updated: stg_olist__customers by adding a deduped window function

--- models/staging/stg_olist__customers.sql
+++ models/staging/stg_olist__customers.sql
@@ -1,7 +1,19 @@
-SELECT
+WITH deduped AS (
+    SELECT
+        customer_id,
+        customer_unique_id,
+        customer_zip_code_prefix,
+        customer_city,
+        customer_state,
+        ROW_NUMBER() OVER (PARTITION BY customer_id ORDER BY _loaded_at DESC) AS row_num
+    FROM {{ source('olist', 'raw_customers') }}
+)
+
+SELECT 
     customer_id,
     customer_unique_id,
     customer_zip_code_prefix,
     customer_city,
     customer_state
-FROM {{source('olist', 'raw_customers')}}
+FROM deduped
+WHERE row_num = 1 
