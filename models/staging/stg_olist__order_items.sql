21e9ab522c12ec6a4aacacd661afbc380a4d18cd
Mark Smolders <marksmolders09@gmail.com>
2026-07-19 15:46:08 +0000

    added: stg_olist__order_items.sql staging model with a window function to remove duplications.

---
+++ models/staging/stg_olist__order_items.sql
@@ -0,0 +1,23 @@
+WITH deduped AS(
+    SELECT 
+        order_id,
+        order_item_id,
+        product_id,
+        seller_id,
+        shipping_limit_date,
+        price,
+        freight_value,
+        ROW_NUMBER() OVER (PARTITION BY order_id, order_item_id ORDER BY _loaded_at DESC) AS row_num
+        FROM {{source('olist', 'raw_order_items')}}
+)
+
+SELECT 
+    order_id,
+    order_item_id,
+    product_id,
+    seller_id,
+    shipping_limit_date,
+    price,
+    freight_value
+FROM deduped
+WHERE row_num = 1
