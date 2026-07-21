e49d30418511b3f734485b31e4cb047f522b4052
Mark Smolders <marksmolders09@gmail.com>
2026-07-21 09:04:10 +0000

    Added: stg_olist__orders staging model with deduplication window function and with Timestamps casted.

---
+++ models/staging/stg_olist__orders.sql
@@ -0,0 +1,25 @@
+WITH deduped AS(
+    SELECT
+        order_id,
+        customer_id,
+        order_status,
+        CAST(order_purchase_timestamp AS TIMESTAMP) AS order_purchase_timestamp,
+        CAST(order_approved_at AS TIMESTAMP) AS order_approved_at,
+        CAST(order_delivered_carrier_date AS TIMESTAMP) AS order_delivered_carrier_date,
+        CAST(order_delivered_customer_date AS TIMESTAMP) AS order_delivered_customer_date,
+        CAST(order_estimated_delivery_date AS TIMESTAMP) AS order_estimated_delivery_date,
+        ROW_NUMBER() OVER (PARTITION BY order_id ORDER BY _loaded_at DESC) AS row_num
+    FROM {{ source('olist', 'raw_orders') }}
+)
+
+SELECT
+    order_id,
+    customer_id,
+    order_status,
+    order_purchase_timestamp,
+    order_approved_at,
+    order_delivered_carrier_date,
+    order_delivered_customer_date,
+    order_estimated_delivery_date
+FROM deduped
+WHERE row_num = 1
