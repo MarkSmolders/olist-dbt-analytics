938309a1a24dc6ea0e39d5b769a7df2ebee815fc
Mark Smolders <marksmolders09@gmail.com>
2026-08-05 15:04:41 +0000

    updated: seller_performance.sql 'cancelled' typo.

--- models/marts/analytics/seller_performance.sql
+++ models/marts/analytics/seller_performance.sql
@@ -7,7 +7,7 @@ SELECT
     SUM(FP.payment_value) AS total_payment_value,
     COUNT(FO.order_item_id) AS total_items_sold,
     AVG(FO.freight_value) AS average_freight_value,
-    COUNT(CASE WHEN FR.order_status = 'cancelled' THEN FO.order_id END) AS cancelled_orders
+    COUNT(CASE WHEN FR.order_status = 'canceled' THEN FO.order_id END) AS cancelled_orders
 FROM {{ref('dim_sellers')}} AS DS
 LEFT JOIN {{ref('fct_order_items')}} AS FO ON DS.seller_id = FO.seller_id
 LEFT JOIN {{ref('fct_order_payments')}} AS FP ON FO.order_id = FP.order_id
