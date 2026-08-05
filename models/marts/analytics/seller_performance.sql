3f1309e7960b51e708f00c1bd6fb397378c806ea
Mark Smolders <marksmolders09@gmail.com>
2026-08-05 14:19:55 +0000

    updated: seller_performance.sql analytical model. Irrelevant columns were removed and new aggregated columns added.

--- models/marts/analytics/seller_performance.sql
+++ models/marts/analytics/seller_performance.sql
@@ -1,15 +1,15 @@
-SELECT
-    FOI.order_id,
-    FOI.order_item_id,
-    FOI.product_id,
-    FOI.seller_id,
+SELECT 
+    DS.seller_id,
+    DS.seller_zip_code_prefix,
     DS.seller_city,
     DS.seller_state,
-    FOI.price,
-    FOI.freight_value,
-    DP.delivery_delay_days,
-    FR.review_score
-FROM {{ref('fct_order_items')}} AS FOI
-LEFT JOIN {{ref('delivery_performance')}} AS DP ON FOI.order_id = DP.order_id
-LEFT JOIN {{ref('fct_order_reviews')}} AS FR ON FOI.order_id = FR.order_id
-LEFT JOIN {{ref('dim_sellers')}} AS DS ON FOI.seller_id = DS.seller_id
+    COUNT(DISTINCT FO.order_id) AS total_orders,
+    SUM(FP.payment_value) AS total_payment_value,
+    COUNT(FO.order_item_id) AS total_items_sold,
+    AVG(FO.freight_value) AS average_freight_value,
+    COUNT(CASE WHEN FR.order_status = 'cancelled' THEN FO.order_id END) AS cancelled_orders
+FROM {{ref('dim_sellers')}} AS DS
+LEFT JOIN {{ref('fct_order_items')}} AS FO ON DS.seller_id = FO.seller_id
+LEFT JOIN {{ref('fct_order_payments')}} AS FP ON FO.order_id = FP.order_id
+LEFT JOIN {{ref('fct_orders')}} AS FR ON FO.order_id = FR.order_id
+GROUP BY DS.seller_id, DS.seller_zip_code_prefix, DS.seller_city, DS.seller_state
